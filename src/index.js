d66904909a9664a6d7cf1ee8f9a29e3497e120d9
LeeHow <iversonwoool@gmail.com>
2022-03-30 19:03:30 +0800

    feat: 管理资源

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import _ from 'lodash'
+import './style.css'
+import Icon from './notion-avatar.png'
 
 function component() {
   const element = document.createElement('div');
@@ -6,6 +8,12 @@ function component() {
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   // element.innerHTML = 'Hello Webpack'
+  element.classList.add('hello')
+
+
+  const img = document.createElement('img')
+  img.src = Icon
+  element.appendChild(img)
   return element;
 }
 
