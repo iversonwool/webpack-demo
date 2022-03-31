9beee12001595749871bfd79c651cba35129ee81
LeeHow <iversonwoool@gmail.com>
2022-03-31 09:57:31 +0800

    feat: 管理输出

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import './style.css'
-import Icon from './notion-avatar.png'
+// import Icon from './notion-avatar.png'
+import printMe from "./print";
 
 function component() {
   const element = document.createElement('div');
@@ -10,10 +11,13 @@ function component() {
   // element.innerHTML = 'Hello Webpack'
   element.classList.add('hello')
 
-
-  const img = document.createElement('img')
-  img.src = Icon
-  element.appendChild(img)
+  const btn = document.createElement('button')
+  btn.innerHTML = 'Click me and check the console!'
+  btn.onclick = printMe
+  element.appendChild(btn)
+  // const img = document.createElement('img')
+  // img.src = Icon
+  // element.appendChild(img)
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js'
+  },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, 'dist')
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
   module: {
     rules: [
       {test: /\.css$/i, use: ['style-loader', 'css-loader']},
       {test: /\.(png|jpg|svg|jpeg|gif)$/i, type: "asset/resource"}
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({title: "管理输出"})
+  ]
 }
